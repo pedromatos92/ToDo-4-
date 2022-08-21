a5d9d9288ec2244ca3635abd5e2c682c358ab3d0
pedromatos92 <peu.matos92@gmail.com>
2022-08-21 01:08:32 -0300

    Todo 4

---
+++ index.js
@@ -0,0 +1,30 @@
+const entrada = require ('prompt-sync')({sigint: true});
+
+let x;
+let y;
+let w;
+
+
+
+x = entrada ("Quem? ");
+
+y = entrada (" O que deseja?");
+
+w = entrada ("Para que ?");
+
+console.log ('como' + " " + `${x}`+ " " + 'desejo um' + " " + `${y}`+ " " + ' para' + " " + `${w}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
